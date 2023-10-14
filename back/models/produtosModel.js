5ce28bdeccc29e9556507fd0ab32e87a8dcdc555
marciopalves <marciopalves89@gmail.com>
2023-10-14 07:16:34 -0300

    Arquivo Model que ficou faltando no ultimo commit.

---
+++ back/models/produtosModel.js
@@ -0,0 +1,22 @@
+const mongoose = require('../db/conn');
+const { Schema } = require('mongoose');
+
+const Produto = mongoose.model(
+    'Produto', new Schema({
+
+        nome:{
+            type: String
+        },
+
+        descricao:{
+            type: String
+        },
+
+        valor:{
+            type: Number
+        }
+    }, {timestamps: true}
+    )
+);
+
+module.exports = Produto;
